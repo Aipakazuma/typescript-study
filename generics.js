9f701709b2b6e402dd71c0266e23e5228d28cb32
aipa <gundamgundam777@gmail.com>
2016-12-08 21:49:43 +0900

    [add] use class.

--- generics.js
+++ generics.js
@@ -1,4 +1,38 @@
-function testGenerics(string) {
+function useGenerics(string) {
     console.log(string);
 }
-testGenerics('Hello, world.');
+// useGenerics<string>('Hello, world.');
+// Operator '+' cannot be applied to types '10' and 'U'.
+// function useMultipleGenerics<T, U>(string: T, number: U): void {
+//   console.log(string);
+//   console.log(10 + number);
+// }
+// これはセーフ
+function useMultipleGenerics(string, number) {
+    console.log(string);
+    console.log(number);
+}
+// useMultipleGenerics<string, number>('Hello', 100);
+var TestModel = (function () {
+    function TestModel(_id, _name, _genericsTest) {
+        this._id = _id;
+        this._name = _name;
+        this._genericsTest = _genericsTest;
+    }
+    TestModel.prototype.id = function () {
+        return this._id;
+    };
+    TestModel.prototype.name = function () {
+        return this._name;
+    };
+    TestModel.prototype.genericsTest = function () {
+        return this._genericsTest;
+    };
+    return TestModel;
+}());
+// var testModel = new TestModel(10, 'Aipa');
+// console.log(testModel.id());
+// console.log(testModel.name());
+// use generics
+var testModel = new TestModel(10, 'Aipa', 'Aipa2');
+console.log(testModel.genericsTest());
--- generics.ts
+++ generics.ts
@@ -1,5 +1,48 @@
-function testGenerics<T>(string: T): void {
+function useGenerics<T>(string: T): void {
   console.log(string);
 }
 
-testGenerics<string>('Hello, world.');
+// useGenerics<string>('Hello, world.');
+
+// Operator '+' cannot be applied to types '10' and 'U'.
+// function useMultipleGenerics<T, U>(string: T, number: U): void {
+//   console.log(string);
+//   console.log(10 + number);
+// }
+
+// これはセーフ
+function useMultipleGenerics<T, U>(string: T, number: U): void {
+	console.log(string);
+	console.log(number);
+}
+
+// useMultipleGenerics<string, number>('Hello', 100);
+
+class TestModel<T> {
+  constructor(
+    private _id: number,
+    private _name: string,
+    private _genericsTest: T
+  ) {
+  }
+
+  id() {
+    return this._id;
+  }
+
+  name() {
+    return this._name;
+  }
+
+  genericsTest() {
+    return this._genericsTest;
+  }
+}
+
+// var testModel = new TestModel(10, 'Aipa');
+// console.log(testModel.id());
+// console.log(testModel.name());
+
+// use generics
+var testModel = new TestModel<string>(10, 'Aipa', 'Aipa2');
+console.log(testModel.genericsTest());
